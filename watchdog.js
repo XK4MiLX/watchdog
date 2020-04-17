ee864e4e4744dc626a060742ecb7abb15c683f3b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-17 07:44:34 +0200

    Update watchdog.js

--- watchdog.js
+++ watchdog.js
@@ -115,7 +115,7 @@ var timestamp = moment.unix(Number(zelbench_time.trim()));
 var bench_local_time = timestamp.format("DD/MM/YYYY HH:mm:ss")  
 console.log('Last benchmark time = \x1b[33m'+bench_local_time+'\x1b[0m');
 var next_benchmark_time = moment(timestamp, 'DD/MM/YYYY HH:mm:ss').add(durationInMinutes, 'minutes').format('DD/MM/YYYY HH:mm:ss');
-console.log('Next benchmark time = \x1b[33m'+bench_local_time+'\x1b[0m');
+console.log('Next benchmark time = \x1b[33m'+next_benchmark_time+'\x1b[0m');
 } 
   
 if (zelcash_last_paid_height.trim() == "null" || zelcash_last_paid_height == ""){
