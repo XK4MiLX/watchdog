b6f351aef505b996275d314ec277b9870c33e9ed
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-05-03 11:32:59 +0200

    better update check

--- watchdog.js
+++ watchdog.js
@@ -47,19 +47,26 @@ console.log('=================================================================')
 
 
 function auto_update() {
-
+ 
  var remote_version = shell.exec("curl -sS https://raw.githubusercontent.com/XK4MiLX/watchdog/master/package.json | jq -r '.version'",{ silent: true }).stdout;
  var local_version = shell.exec("jq -r '.version' package.json",{ silent: true }).stdout;
 
- if ( remote_version.trim() !== local_version.trim() && remote_version != "" ){
-   console.log('\x1b[34mNew version detected:\x1b[0m');
-   console.log('=================================================================');
-   console.log('Local version: \x1b[34m'+local_version.trim()+'\x1b[0m');
-   console.log('Remote version: \x1b[32m'+remote_version.trim()+'\x1b[0m');
-   console.log('\x1b[32mUpdating...\x1b[0m');
-   console.log('=================================================================');
-   shell.exec("git pull",{ silent: true }).stdout;
+ if ( remote_version.trim() != "" && local_version.trim() != "" ){  
+  
+    if ( remote_version.trim() !== local_version.trim() ){
+   
+     console.log('\x1b[34mNew version detected:\x1b[0m');
+     console.log('=================================================================');
+     console.log('Local version: \x1b[34m'+local_version.trim()+'\x1b[0m');
+     console.log('Remote version: \x1b[32m'+remote_version.trim()+'\x1b[0m');
+     console.log('\x1b[32mUpdating...\x1b[0m');
+     console.log('=================================================================');
+     shell.exec("git pull",{ silent: true }).stdout;
+   
+    }
+  
   }
+  
 }
 
 function zeldaemon_check() {
